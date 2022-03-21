eabd64dc95d755ba75550ce5539803158b01b0d7
ekito-line <ekito.line@gmail.com>
2022-03-21 19:52:48 +0900

    html編集

--- snsapp/views.py
+++ snsapp/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post, User
+
+from django.contrib.auth import logout as django_logout
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'snsapp/home.html')
+
+
+@login_required
+def logout(request):
+    django_logout(request)
+    domain = settings.SOCIAL_AUTH_AUTH0_DOMAIN
+    client_id = settings.SOCIAL_AUTH_AUTH0_KEY
+    return_to = 'http://127.0.0.1:8000'  # this can be current domain
+    return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
