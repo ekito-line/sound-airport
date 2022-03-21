d53dba9442868d9c6d2f2eb6b8be9003acdfa640
ekito-line <ekito.line@gmail.com>
2022-03-21 23:19:18 +0900

    modelにsound追加、baseにMEDIA_ROOT追加

--- project/settings/base.py
+++ project/settings/base.py
@@ -107,6 +107,8 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- snsapp/forms.py
+++ snsapp/forms.py
@@ -10,5 +10,5 @@ class PostForm(forms.ModelForm):
         fields = (
             'icon',
             'title',
-            # 'sound',
+            'sound',
         )
