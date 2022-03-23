ec2d59968d3794010b40f3e8792c24065fe59a3d
FurinkazanPlanet <fushidaze2424@gmail.com>
2022-03-23 12:06:24 +0900

    follow, like model

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('logout/', views.logout, name='logout'),
+    path('post/', views.post, name='post'),
+    path('main/', views.main, name='main'),
+    path('<int:user_id>/follow/', views.follow, name='follow'),
+    path('<int:post_id>/like/', views.like, name='like'),
 ]
--- snsapp/views.py
+++ snsapp/views.py
@@ -1,16 +1,18 @@
-from django.shortcuts import render, redirect
-from .models import Post, User
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post, User, Follow, Like
+from .forms import PostForm, FollowForm, LikeForm
+from django.core.paginator import Paginator
 
 from django.contrib.auth import logout as django_logout
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from datetime import date
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'snsapp/home.html')
-
 
 @login_required
 def logout(request):
@@ -19,3 +21,70 @@ def logout(request):
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
     return_to = 'https://sound-airport.azurewebsites.net'  # this can be current domain
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
+
+#投稿用の処理
+# @login_required(login_url='/snsapp/login/')
+def post(request):
+    params = {
+        'form': PostForm(),
+    }
+
+    if (request.method == 'POST'):
+        title = request.POST['title']
+        creator = request.user
+        publish_date = date.today()
+        post = Post(title = title, creator = creator, publish_date = publish_date)
+        post.save()
+        return redirect(to = '/main')
+    return render(request, 'snsapp/post.html', params)
+
+#Mainに投稿を表示する
+# @login_required(login_url='/login/')
+def main(request):
+    user = request.user #これでuserが取れているのか不安。creatorかな
+    posts = Post.objects.all()
+
+    params = {
+        'data' : posts,
+        'user' : user,
+    }
+
+    return render(request, 'snsapp/main.html', params)
+
+#followするための関数
+# @login_required(login_url='/login/')
+def follow(request, user_id):
+    params = {
+        'form' : FollowForm(),
+    }
+
+    follow_user = get_object_or_404(User, pk=user_id)
+
+    if (request.method == 'POST'):
+        creator = request.user
+        follow_target = follow_user
+        follow = Follow(creator = creator, follow_target = follow_target)
+        follow.save()
+        return redirect(to = '/main')
+
+    return render(request, 'snsapp/main.html', params)
+
+#Likeするための関数
+# @login_required(login_url='/accounts/login/')
+def like(request, post_id):
+    params = {
+        'form' : LikeForm(),
+    }
+
+    target_post = get_object_or_404(Post, pk=post_id)
+
+    if (request.method == 'POST'):
+        creator = request.user
+        target_post = target_post
+        like = Like(creator = creator, target_post = target_post)
+        like.save()
+        return redirect(to = '/main')
+
+    return render(request, 'snsapp/main.html', params)
+
+#paginationに必要なもの
