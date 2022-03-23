9bf0f873bfe00190353d4f7b1a8a08bb2ec67425
FurinkazanPlanet <fushidaze2424@gmail.com>
2022-03-23 22:45:49 +0900

    home, startの調整

--- snsapp/urls.py
+++ snsapp/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('start/', views.start, name='start'),
     path('logout/', views.logout, name='logout'),
     path('post/', views.post, name='post'),
-    path('main/', views.main, name='main'),
+    path('home/', views.home, name='home'),
     path('<int:user_id>/follow/', views.follow, name='follow'),
     path('<int:post_id>/like/', views.like, name='like'),
     path('mypage/', views.mypage, name='mypage'),
